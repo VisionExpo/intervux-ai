e8fe896f71816bc6c3251756626c15354fd85d61
Vision-Expo <gorulevishal984@gmail.com>
2026-02-03 17:41:05 +0530

    feat: send temporary audio response from backend for interviewer speech

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 import json
+import numpy as np
 
 app = FastAPI()
 
@@ -15,26 +16,44 @@ app.add_middleware(
 @app.websocket("/ws/interview")
 async def interview_socket(ws: WebSocket):
     await ws.accept()
-    print("Client connected")
+    print("✅Client connected")
 
     try:
         while True:
             message = await ws.receive()
 
+            # 🎧 Audio from Frontend
             if "bytes" in message:
-                # Audio chunk received
-                print(f"Audio chunk size: {len(message['bytes'])}")
-
+                pass
+            
+            # 🧠 Text / Events
             if "text" in message:
                 data = json.loads(message["text"])
                 print("Received:",data)
 
-                # Fake AI response (TEMP)
+                # Temp AI response (TEMP)
                 await ws.send_text(json.dumps({
                     "type": "avatar_sync",
                     "text": "Hello, I am your interviewer. Tell me about yourself.",
-                    "emotion": "neutral"
                 }))
 
+                # 🔊 Temp audio responce(Beep)
+                audio_bytes = generate_beep()
+                await ws.send_bytes(audio_bytes)
+
     except WebSocketDisconnect:
-        print("Client disconnected")
+        print("❌Client disconnected")
+
+
+def generate_beep(
+        duration_sec: float = 0.4,
+        frequency: float = 440.0,
+        sample_rate: int = 44100
+) -> bytes:
+    """
+    Generate a short sine-wave beep as Float32 PCM
+    """
+    t =  np.linspace(0, duration_sec, int(sample_rate*duration_sec), False)
+    tone = 0.3 * np.sin(2 * np.pi * frequency * t)
+
+    return tone.astype(np.float32).tobytes()
